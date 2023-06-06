0f51cb144c6488f31b3d294f1f33ee8e1f3d48d2
Ivan <93859862+jshuckbot@users.noreply.github.com>
2023-06-06 18:21:32 +0700

    Lesson 5 (#5)
    ready lesson 5

---
+++ lesson_5/task_1.py
@@ -0,0 +1,14 @@
+import os
+
+
+def get_absolute_path(path):
+    *_, extension = os.path.splitext(path)
+    path_to, _ = os.path.split(path)
+    file_name = os.path.basename(path).split('.')[0]
+    
+    return path_to, file_name, extension
+
+
+if __name__ == '__main__':
+    path = '/Users/jshuckbot/projects/geekbrains/python_basics/lesson_5/ivan.txt'
+    print(get_absolute_path(path))
